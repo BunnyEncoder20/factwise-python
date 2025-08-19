ebb18856319e49ddbc1fa90c7894535bc8c94eb5
Varun Verma <varun.verma2024@gmail.com>
2025-08-19 22:44:03 +0530

    add: impl/UserManager and create_user

---
+++ impl/user_manager.py
@@ -0,0 +1,24 @@
+import json
+from user_base import UserBase
+from utils.file_db import FileDB
+
+class UserManager(UserBase):
+    def __init__(self, db_path='db/users.json'):
+        self.db = FileDB(db_path)
+
+    # TODO: Check this param name mismatch
+    def create_user(self, json_str: str) -> str:
+        data = json.loads(json_str)
+        users = self.db.read()
+
+        user_id = data.get("id")
+        if not user_id:
+            raise ValueError("User Id is required")
+
+        if user_id in users:
+            raise ValueError(f"User with id:[{user_id}] already exists")
+
+        users[user_id] = data
+        self.db.write(users)
+
+        return json.dumps({"status": "success", "user": data})

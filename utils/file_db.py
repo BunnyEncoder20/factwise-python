dbca5e7b2a545f8249f0070a69d08e73a8b835da
Varun Verma <varun.verma2024@gmail.com>
2025-08-20 21:43:48 +0530

    Update file_db.py

--- utils/file_db.py
+++ utils/file_db.py
@@ -9,14 +9,14 @@ class FileDB:
         self.path = path
         os.makedirs(os.path.dirname(path), exist_ok=True)
         if not os.path.exists(path):
-            with open(path, 'w') as f:
-                json.dump({}, f)
+            with open(path, 'w') as file:
+                json.dump({}, file)
 
     def read(self) -> Dict[str, Any]:
         # if not os.path.exists(self.path): return {}
-        with open(self.path, 'r') as f:
-            return json.load(f)
+        with open(self.path, 'r') as file:
+            return json.load(file)
 
     def write(self, data: Dict[str, Any]) -> None:
-        with open(self.path, 'w') as f:
-            json.dump(data, f, indent=4)
+        with open(self.path, 'w') as file:
+            json.dump(data, file, indent=4)

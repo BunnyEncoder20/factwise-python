a392de4d46cebd8c492bfdb9bbec0c27b8400c7c
Varun Verma <varun.verma2024@gmail.com>
2025-08-21 22:40:31 +0530

    create: team pydantic schemas

---
+++ app/schemas/team_schemas.py
@@ -0,0 +1,60 @@
+# Schemas for Team management
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class CreateTeamRequest(BaseModel):
+    name: str = Field(max_length=64)
+    description: Optional[str] = Field(default = None, max_length=128)
+    admin: str
+
+class CreateTeamResponse(BaseModel):
+    id: str
+
+class TeamInfo(BaseModel):
+    name: str
+    description: str
+    creation_time: str
+    admin: str
+
+class ListTeamsResponse(BaseModel):
+    __root__: List[TeamInfo]
+
+class DescribeTeamRequest(BaseModel):
+    id: str
+
+class DescribeTeamResponse(BaseModel):
+    name: str
+    description: str
+    creation_time: str
+    admin: str
+
+class UpdateTeamPayload(BaseModel):
+    name: Optional[str] = Field(max_length=64)
+    description: Optional[str] = Field(max_length=128)
+    admin: Optional[str] = None
+
+class UpdateTeamRequest(BaseModel):
+    id: str
+    team: UpdateTeamPayload
+
+class UpdateTeamResponse(BaseModel):
+    status: str
+
+class AddUsersToTeamRequest(BaseModel):
+    id: str
+    users: List[str]
+
+class RemoveUsersFromTeamRequest(BaseModel):
+    id: str
+    users: List[str]
+
+class TeamUser(BaseModel):
+    id: str
+    name: str
+    display_name: str
+
+class ListTeamUsersRequest(BaseModel):
+    id: str
+
+class ListTeamUsersResponse(BaseModel):
+    __root__: List[TeamUser]

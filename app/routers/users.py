130e9e2f1cadefa68f86fc9fb994b99c23809c09
Varun Verma <varun.verma2024@gmail.com>
2025-08-21 19:49:26 +0530

    update user.py | add describe, update, get_user_teams routes

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,17 +2,32 @@ import json
 from fastapi import APIRouter, Depends
 
 from dependencies import get_user_manager
-from schemas import CreateUserRequest, CreateUserResponse, ListUsersResponse
+from schemas import user_schemas as model
 from impl.user_manager import UserManager
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/create", response_model=CreateUserResponse)
-def create_user(req: CreateUserRequest, manager: UserManager = Depends(get_user_manager)):
+@router.post("/create", response_model=model.CreateUserRequest)
+def create_user(req: model.CreateUserRequest, manager: UserManager = Depends(get_user_manager)):
     response_json = manager.create_user(req.model_dump_json())
     return json.loads(response_json)
 
-@router.get("/list", response_model=ListUsersResponse)
+@router.get("/list", response_model=model.ListUsersResponse)
 def list_users(manager: UserManager = Depends(get_user_manager)):
     response_json = manager.list_users()
+    return json.loads(response_json)
+
+@router.get("/describe", response_model=model.DescribeUserResponse)
+def describe_user(req: model.DescribeUserRequest, manager: UserManager = Depends(get_user_manager)):
+    response_json = manager.describe_user(req.model_dump_json())
+    return json.loads(response_json)
+
+@router.post("/update")
+def update_user(req: model.UpdateUserRequest, manager: UserManager = Depends(get_user_manager)):
+    response_json = manager.update_user(req.model_dump_json())
+    return json.loads(response_json)
+
+@router.get("/get_teams", response_model=model.GetUserTeamsResponse)
+def get_user_teams(req: model.GetUserTeamsRequest, manager: UserManager = Depends(get_user_manager)):
+    response_json = manager.get_user_teams(req.model_dump_json())
     return json.loads(response_json)
